e2135e0de0339dbba4a3327563b20b03b5d7c126
claudiogaudiano <gaudicla10@gmail.com>
2025-02-09 17:30:30 +0100

    Update PrivateRoute.js

--- Frontend/src/PrivateRoute.js
+++ Frontend/src/PrivateRoute.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Navigate, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import { getCookie } from "./cookieFunction";
+import Login from './components/Login';
 
 export default function PrivateRoute({ totSpese, totGuadagni }) {
     const token = getCookie("User");
     return token ?
         <Outlet context={{ totSpese, totGuadagni }} />
-        : <Navigate to="/"></Navigate>;
-
+        : <Login />;
 }
